062c93fa8496a4325f087846a516af0b1b888510
Java sauce <mohammedbrandon204@gmail.com>
2026-07-04 20:35:05 -0400

    Remove duplicate spawner. Compress sounds
    Also add NEI style tooltips. Sounds are compressed to save file size
    
    command is:
    ffmpeg -i file.ogg -qscale:a 1 -f ogg output.ogg
    
    I had to use 256k for the evilcreature jump sound effect as that command made it sound too quiet

--- src/main/java/jerios/morecreeps/item/ItemMonsterSpawner.java
+++ src/main/java/jerios/morecreeps/item/ItemMonsterSpawner.java
@@ -2,19 +2,19 @@ package jerios.morecreeps.item;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import jerios.morecreeps.CREEPSConstants;
-import jerios.morecreeps.registry.CREEPSItemBlocks;
 import jerios.morecreeps.registry.TabsManager;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Facing;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
@@ -39,6 +39,16 @@ public class ItemMonsterSpawner extends ItemBlock {
         return s;
     }
 
+    public String getEntityNameOnly(ItemStack p_77653_1_) {
+        String s1 = CreepSpawnEggItem.INTEGER_STRING_MAP.get(p_77653_1_.getItemDamage());
+
+        if (s1 != null) {
+            return StatCollector.translateToLocal("entity." + s1 + ".name");
+        }
+
+        return null;
+    }
+
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int facing, float unknown, float unkown2, float unkown3) {
 
@@ -56,9 +66,6 @@ public class ItemMonsterSpawner extends ItemBlock {
                 String name = CreepSpawnEggItem.STRING_ID_MAP.get(stack.getItemDamage());
                 if (name != null) {
                     tileentitymobspawner.func_145881_a().setEntityName(name);
-                } else {
-                    System.out.println("YOOOOO " + stack.getItemDamage());
-                    System.out.println(CreepSpawnEggItem.STRING_ID_MAP.keySet());
                 }
             }
 
@@ -72,10 +79,23 @@ public class ItemMonsterSpawner extends ItemBlock {
     }
 
 
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> list, boolean p_77624_4_) {
+
+        EntityLiving entity = CreepSpawnEggItem.createEntityByID(stack.getItemDamage(), player.worldObj);
+
+        if (entity instanceof IMob && getEntityNameOnly(stack) != null) {
+            list.add(EnumChatFormatting.RED + getEntityNameOnly(stack));
+        } else {
+            list.add(EnumChatFormatting.BLUE + getEntityNameOnly(stack));
+        }
+
+        super.addInformation(stack, player, list, p_77624_4_);
+    }
+
     @SideOnly(Side.CLIENT)
     @Override
     public void getSubItems(Item item, CreativeTabs tab, List<ItemStack> stacks) {
-        super.getSubItems(item, tab, stacks);
         for (CreepSpawnEggItem.EGGSpots entityegginfo : CreepSpawnEggItem.INTEGER_COLOR_MAP.values()) {
             stacks.add(new ItemStack(item, 1, entityegginfo.id));
         }
